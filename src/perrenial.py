ea55dcde23d23845583a98cec5edc8b7c23f579c
vorozhkog <115161827+vorozhkog@users.noreply.github.com>
2023-05-29 14:37:55 +0100

    Update perrenial.py

--- src/perrenial.py
+++ src/perrenial.py
@@ -1,9 +1,8 @@
 import os
 from dotenv import load_dotenv
-from collections import defaultdict
 import supervisely as sly
-import json
 import xml.etree.ElementTree as ET
+import xmltodict
 
 # https://osf.io/mh9sj/
 
@@ -28,13 +27,6 @@ dataset_voc_path = (
 #     r"C:\Users\German\Documents\perrenial plants\coco_annotations\coco_annotations\json_annotation_val.json",
 # ]
 
-
-def etree_to_dict(t):
-    if type(t) is ET.ElementTree:
-        return etree_to_dict(t.getroot())
-    return {**t.attrib, "text": t.text, **{e.tag: etree_to_dict(e) for e in t}}
-
-
 # create project and initialize meta
 project_name = os.path.basename(os.path.dirname(os.path.dirname(dataset_voc_path)))
 project = api.project.get_info_by_name(workspace.id, project_name)
@@ -44,52 +36,62 @@ project = api.project.create(workspace.id, project_name)
 meta = sly.ProjectMeta()
 
 
-def nested_dict_pairs_iterator(dict_obj):
-    """This function accepts a nested dictionary as argument
-    and iterate over all values of nested dictionaries
-    """
-    # Iterate over all key-value pairs of dict argument
-    for key, value in dict_obj.items():
-        # Check if value is of dict type
-        if isinstance(value, dict):
-            # If value is dict then iterate over all its values
-            for pair in nested_dict_pairs_iterator(value):
-                yield (key, *pair)
-        else:
-            # If value is not dict type then yield the value
-            yield (key, value)
-
-
 # create dataset and iterate over annotation paths
 dataset_voc = api.dataset.create(project.id, os.path.basename(dataset_voc_path))
 voc_ann_paths = sly.fs.list_files(dataset_voc_path)
 for path in voc_ann_paths:
     tree = ET.parse(path)
-    dict_xml = etree_to_dict(tree.getroot())
-    for pair in nested_dict_pairs_iterator(dict_xml):
-        print(pair)
+    xml_data = tree.getroot()
+    xmlstr = ET.tostring(xml_data, encoding="utf-8", method="xml")
+
+    data_dict = dict(xmltodict.parse(xmlstr))
     # get image path and upload
     image_path = os.path.join(
         r"C:\Users\German\Documents\perrenial plants\raw_images\raw_images",
-        dict_xml["filename"]["text"],
+        data_dict["annotation"]["filename"],
     )
     image_info = api.image.upload_path(
-        dataset_voc.id, dict_xml["filename"]["text"], image_path
+        dataset_voc.id, data_dict["annotation"]["filename"], image_path
     )
 
     labels = []
-    for k, v in dict_xml.items():
-        if k != "object":
-            continue
-        # get bbox cords
-        xmin = int(dict_xml["object"]["bndbox"]["xmin"]["text"])
-        ymin = int(dict_xml["object"]["bndbox"]["ymin"]["text"])
-        xmax = int(dict_xml["object"]["bndbox"]["xmax"]["text"])
-        ymax = int(dict_xml["object"]["bndbox"]["ymax"]["text"])
 
-        # get class name, create labels
+    if type(data_dict["annotation"]["object"]) == list:
+        for obj in data_dict["annotation"]["object"]:
+            # get bbox cords
+            xmin = int(obj["bndbox"]["xmin"])
+            ymin = int(obj["bndbox"]["ymin"])
+            xmax = int(obj["bndbox"]["xmax"])
+            ymax = int(obj["bndbox"]["ymax"])
+
+            # get class name, create labels
+            bbox = sly.Rectangle(top=ymin, left=xmin, bottom=ymax, right=xmax)
+            class_name = obj["name"]
+            obj_class = meta.get_obj_class(class_name)
+            if obj_class is None:
+                obj_class = sly.ObjClass(class_name, sly.Rectangle)
+                meta = meta.add_obj_class(obj_class)
+                api.project.update_meta(project.id, meta)
+            label = sly.Label(bbox, obj_class)
+            labels.append(label)
+
+        # upload annotation
+        ann = sly.Annotation(
+            img_size=[
+                int(data_dict["annotation"]["size"]["height"]),
+                int(data_dict["annotation"]["size"]["width"]),
+            ],
+            labels=labels,
+        )
+        api.annotation.upload_ann(image_info.id, ann)
+        print(f"uploaded bbox to image(id:{image_info.id})")
+    else:
+        xmin = int(data_dict["annotation"]["object"]["bndbox"]["xmin"])
+        ymin = int(data_dict["annotation"]["object"]["bndbox"]["ymin"])
+        xmax = int(data_dict["annotation"]["object"]["bndbox"]["xmax"])
+        ymax = int(data_dict["annotation"]["object"]["bndbox"]["ymax"])
         bbox = sly.Rectangle(top=ymin, left=xmin, bottom=ymax, right=xmax)
-        class_name = dict_xml["object"]["name"]["text"]
+        class_name = data_dict["annotation"]["object"]["name"]
         obj_class = meta.get_obj_class(class_name)
         if obj_class is None:
             obj_class = sly.ObjClass(class_name, sly.Rectangle)
@@ -98,14 +100,14 @@ for path in voc_ann_paths:
         label = sly.Label(bbox, obj_class)
         labels.append(label)
 
-    # upload annotation
-    ann = sly.Annotation(
-        img_size=[
-            int(dict_xml["size"]["height"]["text"]),
-            int(dict_xml["size"]["width"]["text"]),
-        ],
-        labels=labels,
-    )
-    api.annotation.upload_ann(image_info.id, ann)
-    print(f"uploaded bbox to image(id:{image_info.id})")
+        # upload annotation
+        ann = sly.Annotation(
+            img_size=[
+                int(data_dict["annotation"]["size"]["height"]),
+                int(data_dict["annotation"]["size"]["width"]),
+            ],
+            labels=labels,
+        )
+        api.annotation.upload_ann(image_info.id, ann)
+        print(f"uploaded bbox to image(id:{image_info.id})")
 print(f"Dataset {dataset_voc.id} has been successfully created.")
